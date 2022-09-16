993aabbfe6d422db372caa31142ca25b7ed1379e
Prasanna Kumar <kprasanna331@gmail.com>
2022-09-16 17:40:17 +0530

    Routing array created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddBrandComponent } from './add-brand/add-brand.component';
+import { DisplayBrandsComponent } from './display-brands/display-brands.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: AddBrandComponent },
+  { path: "display-brands", component: DisplayBrandsComponent },
+  { path: "edit-brand/:id", component: AddBrandComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
