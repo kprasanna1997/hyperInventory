87023f3ac7dfabd12c407832d509f3ba608ba362
Prasanna Kumar <kprasanna331@gmail.com>
2022-09-16 14:31:53 +0530

    Basic styling and modal creation completed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './Shared/header/header.component';
+import { AddBrandComponent } from './add-brand/add-brand.component';
+import { BrandAddFormComponent } from './brand-add-form/brand-add-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddBrandComponent,
+    BrandAddFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
