d36b87eccd0fca9daf6b444ed31ab912af98aca7
Prasanna Kumar <kprasanna331@gmail.com>
2022-09-16 18:24:47 +0530

    EditBrand method created.

--- src/app/Services/http.service.ts
+++ src/app/Services/http.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
 import { Brand } from '../interface/brand';
 
 @Injectable({
@@ -10,17 +11,27 @@ export class HttpService {
 
   url = "https://devapi.hyperinvento.com"
   companyId = "d2ea8d62-786f-410e-acf5-7c2d72b17deb"
-  access_token: 'KUsb7DoQXbZQPHJHMU2NfMhNZ2XShi'
 
-  
+
+
   constructor(private http: HttpClient) { }
 
   addBrand(brand: Brand) {
-    return this.http.post(`${this.url}/v1/companies/${this.companyId}/brands`, brand)
+    return this.http.post<Brand>(`${this.url}/v1/companies/${this.companyId}/brands`, brand).pipe(map((res) => {
+      console.log(res);
+    }))
   }
 
   getBrandDetails() {
     return this.http.get<any>(`${this.url}/v1/companies/${this.companyId}/brands`)
   }
-  
+
+  deleteBrand(id: string) {
+    return this.http.delete<any>(`${this.url}/v1/brands/${id}/delete`)
+  }
+
+  editBrand(brand: Brand) {
+    return this.http.put<Brand>(`${this.url}/v1/brands/${brand.id}`, brand)
+  }
 }
+
