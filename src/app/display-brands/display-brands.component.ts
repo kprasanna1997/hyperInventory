59ad869c08e7f3cd328fa1649084d93c5e274303
Prasanna Kumar <kprasanna331@gmail.com>
2022-09-16 15:45:49 +0530

    Display brands component created.

---
+++ src/app/display-brands/display-brands.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Brand } from '../interface/brand';
+import { HttpService } from '../Services/http.service';
+
+@Component({
+  selector: 'app-display-brands',
+  templateUrl: './display-brands.component.html',
+  styleUrls: ['./display-brands.component.css']
+})
+export class DisplayBrandsComponent implements OnInit {
+
+  brandLists:Brand[]=[];
+  errorMessage:string="";
+
+  constructor(private httpService:HttpService) { }
+
+  ngOnInit(): void {
+    this.httpService.getBrandDetails().subscribe((brandLists)=>{
+      this.brandLists=brandLists;
+    })
+  }
+
+}
