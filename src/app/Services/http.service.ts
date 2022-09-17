7c4577afdbf6ab52fbebff6c68bdcd6b4fd9d614
Prasanna Kumar <kprasanna331@gmail.com>
2022-09-17 11:15:25 +0530

    Renamed getBrandDetails method to getBrands.

--- src/app/Services/http.service.ts
+++ src/app/Services/http.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators'
 import { Brand } from '../interface/brand';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  url = "https://devapi.hyperinvento.com"
+  // url = "https://devapi.hyperinvento.com"
   companyId = "d2ea8d62-786f-410e-acf5-7c2d72b17deb"
 
 
@@ -17,21 +18,21 @@ export class HttpService {
   constructor(private http: HttpClient) { }
 
   addBrand(brand: Brand) {
-    return this.http.post<Brand>(`${this.url}/v1/companies/${this.companyId}/brands`, brand).pipe(map((res) => {
-      console.log(res);
-    }))
+    return this.http.post<Brand>(`${environment.apiUrl}/v1/companies/${this.companyId}/brands`, brand)
   }
 
-  getBrandDetails() {
-    return this.http.get<any>(`${this.url}/v1/companies/${this.companyId}/brands`)
+  getBrands() {
+    return this.http.get<any>(`${environment.apiUrl}/v1/companies/${this.companyId}/brands`)
   }
 
-  deleteBrand(id: string) {
-    return this.http.delete<any>(`${this.url}/v1/brands/${id}/delete`)
+  deleteBrand(brand: Brand) {
+    console.log(brand);
+
+    return this.http.post<Brand>(`${environment.apiUrl}/v1/brands/${brand.id}/delete`, brand)
   }
 
   editBrand(brand: Brand) {
-    return this.http.put<Brand>(`${this.url}/v1/brands/${brand.id}`, brand)
+    return this.http.put<Brand>(`${environment.apiUrl}/v1/brands/${brand.id}`, brand)
   }
 }
 
